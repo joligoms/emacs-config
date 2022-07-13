13031ae3ed671f5cada8ce87d0caaaced4ede317
João Linhares Gomes <jgomes@pdmfc.com>
2022-07-13 16:19:52 +0100

    other adjusts

--- init.el
+++ init.el
@@ -13,9 +13,7 @@
 (setq-default frame-title-format '("%f [%m]"))
 
 ;; Change font
-;; (set-frame-font "Hack 10" nil t)       
 (set-face-attribute 'default nil :family "Hack" :height 95 :weight 'regular)
-
 
 ;; Turn off tabs indentation
 (setq-default indent-tabs-mode nil)
@@ -29,10 +27,6 @@
 ;; Customize cursor
 (setq-default cursor-type 'hollow)
 
-;; Numbered lines
-;(global-display-line-numbers-mode t)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
 (defun my-terminal-visible-bell ()
       "A friendlier visual bell effect."
       (invert-face 'mode-line)
@@ -42,8 +36,8 @@
       ring-bell-function #'my-terminal-visible-bell)
 
 ;; Set transparent background
-(set-frame-parameter (selected-frame) 'alpha '(98 85))
-(add-to-list 'default-frame-alist '(alpha 98 85))
+(set-frame-parameter (selected-frame) 'alpha '(98))
+(add-to-list 'default-frame-alist '(alpha 98))
 
 ;; Create custom file
 (defconst custom-file (expand-file-name "custom.el" user-emacs-directory))
@@ -143,8 +137,9 @@
   :custom
   (company-idle-delay 0)
   (company-dabbrev-downcase nil)
-  (company-show-numbers t)
   (company-tooltip-limit 10)
+  (company-show-numbers nil)
+
   :hook (after-init . global-company-mode)
   :config
 
@@ -184,24 +179,6 @@
 (use-package dumb-jump
   :config
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
-
-(setq vue-mode-packages
-  '(vue-mode))
-
-(setq vue-mode-excluded-packages '())
-
-(defun vue-mode/init-vue-mode ()
-  "Initialize my package"
-  (use-package vue-mode))
-
-;; (use-package vue-mode
-;;   :mode "\\.vue\\'"
-;;   :config
-;;   (add-hook 'vue-mode-hook #'lsp))
-
-;; (use-package helm)
-
-;; (use-package helm-ag)
 
 (use-package ivy
   :diminish
@@ -279,6 +256,37 @@
   (setq web-mode-indent-style 4))
 
 
+;; Vue mode
+(setq vue-mode-packages
+  '(vue-mode))
+
+(setq vue-mode-excluded-packages '())
+
+(defun vue-mode/init-vue-mode ()
+  "Initialize my package"
+  (use-package vue-mode
+    :config
+    (add-hook 'vue-mode-hook 'display-line-numbers-mode)
+    (add-hook 'sgml-mode-hook 'display-line-numbers-mode)))
+
+;; Remove ugly background from vue mode
+(add-hook 'mmm-mode-hook
+          (lambda ()
+            (set-face-background 'mmm-default-submode-face nil)))
+
+(use-package emmet-mode
+  :diminish
+  :hook ((sgml-mode)
+         (css-mode))
+  :config
+  (defun emmet-tab ()
+    (interactive)
+    (if (looking-at "\\_>")
+        (call-interactively 'emmet-expand-line)
+      (indent-according-to-mode)))
+  (define-key emmet-mode-keymap (kbd "<tab>") 'emmet-tab)
+  (define-key emmet-mode-keymap (kbd "C-j") #'newline-and-indent))
+
 ;; Apparently identation guides on emacs aren't that great...
 ;; (use-package highlight-indent-guides
 ;;   :hook (prog-mode . highlight-indent-guides-mode)
@@ -291,6 +299,9 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+;; Make C-j indent newline
+(global-set-key (kbd "C-j") #'newline-and-indent)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Numbered lines
@@ -299,12 +310,19 @@
 ;; Enable electric pair mode for braces editing
 (electric-pair-mode)
 
+;; Disable automatic indentation
+(setq-default electric-indent-inhibit t)
+
 ;; Show matching braces pairs
 (show-paren-mode 1)
 
-;; Don't show "ARev" and "ELDoc" in modeline
+;; Replace highlighted text with what is typed
+(delete-selection-mode 1)
+
+;; Don't show a couple of default modes in modeline
 (diminish 'auto-revert-mode)
 (diminish 'eldoc-mode)
+(diminish 'mmm-mode)
 
 ;; Load theme
 (load-theme 'doom-moonlight t)
@@ -314,3 +332,5 @@
 (customize-set-variable 'treemacs-user-mode-line-format 'none)
 
 ;; TODO: migrate to Org.mode
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
